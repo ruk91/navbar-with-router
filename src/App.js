cfeaa49943c1fbb6a988ad50941592a12eff6e51
ruk91 <sajithkasn@gmail.com>
2022-05-25 00:09:03 +0530

    ✨ Add active tab dynamically

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import Navbar from './Navbar';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
-  return <Navbar />
+  let component
+
+  // eslint-disable-next-line default-case
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />
+      break
+    case "/pricing":
+      component = <Pricing />
+      break
+    case "/about":
+      component = <About />
+  }
+  return (
+    <>
+      <Navbar />
+      <div className='container'>
+        {component}
+      </div>
+    </>
+
+  )
 }
 
 export default App;
+
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,13 +5,18 @@ export default function Navbar() {
                 Site name
             </a>
             <ul>
-                <li className="active">
-                    <a href="/pricing">Pricing</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
+                <CustomLink href="/pricing">Pricing</CustomLink>
+                <CustomLink href="/about">About</CustomLink>
             </ul>
         </nav>
+    )
+}
+
+function CustomLink({ href, children, ...props }) {
+    const path = window.location.pathname
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href}>{children}</a>
+        </li>
     )
 }
